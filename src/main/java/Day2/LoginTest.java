19aadf8db46331d2131af0acbfd0cfd7c604e779
Devender Nainwal <devendernainwal15@gmail.com>
2025-07-18 01:13:15 +0530

    add Day2 files

---
+++ src/main/java/Day2/LoginTest.java
@@ -0,0 +1,28 @@
+package Day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    public static void main(String[] args) {
+        //Step 1: Set the path to ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ADMIN\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+
+        //Step 2: Create WebDriver instance
+        WebDriver driver = new ChromeDriver();
+
+        //Step 3: Open URL
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        //print page title after login
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+    }
+}
