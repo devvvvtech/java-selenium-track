7214b82fcf5fb48cbae1d5e7f652f2bdd1dd5b4f
Devender Nainwal <devendernainwal15@gmail.com>
2025-07-18 22:26:07 +0530

    modified LoginTest file

--- src/main/java/Day2/LoginTest.java
+++ src/main/java/Day2/LoginTest.java
@@ -16,13 +16,11 @@ public class LoginTest {
         //Step 3: Open URL
         driver.get("https://www.saucedemo.com/");
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
         //print page title after login
         System.out.println(driver.getTitle());
         driver.quit();
-
     }
 }
