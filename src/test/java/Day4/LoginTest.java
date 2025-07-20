169b93ad65670d6e3ddbb59217fe1cb005234779
Devender Nainwal <devendernainwal15@gmail.com>
2025-07-21 01:00:45 +0530

    modified and add some files

---
+++ src/test/java/Day4/LoginTest.java
@@ -0,0 +1,38 @@
+package Day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrom.driver", "C:\\Users\\ADMIN\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+    }
+
+    @Test
+    public void testValidLogin() {
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, "Swag Labs"); //Assetion
+    }
+
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+}
