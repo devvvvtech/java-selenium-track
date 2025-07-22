cc0d8109aa77d1c82c28ce8d0d3dd3b86472a9ca
Devender Nainwal <devendernainwal15@gmail.com>
2025-07-23 00:40:04 +0530

    modified LoginPage.java file

--- src/test/java/Day5/LoginPage.java
+++ src/test/java/Day5/LoginPage.java
@@ -1,36 +1,34 @@
 package Day5;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     WebDriver driver;
 
-    //constructor to pass the WebDriver from the test
+    //All locators declared up top
+    @FindBy(id = "user-name")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    WebElement loginButton;
+
+    //constructor + init
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-
-    //Locator + Actions (combine them)
-    public void enterUsername(String username) {
-        driver.findElement(By.id("user-name")).sendKeys(username);
-    }
-
-    public void enterPassword(String password) {
-        driver.findElement(By.id("password")).sendKeys(password);
-    }
-
-    public void clickLogin() {
-        driver.findElement(By.id("login-button")).click();
-    }
-
-
-    //Or: Combine them all into one method
+    //Action Method
     public void login(String username, String password) {
-        enterUsername(username);
-        enterPassword(password);
-        clickLogin();
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
     }
 
     public boolean isLoginSuccessful() {
