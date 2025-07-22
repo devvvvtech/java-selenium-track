e934389d30171f2d6f0163316e95fac63e8438a8
Devender Nainwal <devendernainwal15@gmail.com>
2025-07-22 22:02:29 +0530

    add LoginPage.java file

---
+++ src/test/java/Day5/LoginPage.java
@@ -0,0 +1,35 @@
+package Day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    //constructor to pass the WebDriver from the test
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    //Locator + Actions (combine them)
+    public void enterUsername(String username) {
+        driver.findElement(By.id("username")).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(By.id("password")).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(By.id("loginButton")).click();
+    }
+
+
+    //Or: Combine them all into one method
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+}
